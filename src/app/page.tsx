928c5b09d7e868deea2e796469908304e97abba5
Abhinav Mishra <abhinavmishra3322@gmail.com>
2025-01-09 23:29:25 +0530

    Add DotPattern component, glowing stars background, and Tailwind CSS animations

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,24 @@
+import { BackgroundGradientAnimation } from '@/components/ui/background-gradient-animation'
+import DotPattern from '@/components/ui/dot-pattern'
 import Particles from '@/components/ui/particles'
 import { VelocityScroll } from '@/components/ui/scroll-based-velocity'
+import { cn } from '@/lib/utils'
 import React from 'react'
 
 const Home = () => {
   return (
     <>
-      <div className='relative flex items-center justify-center h-screen w-screen'>
-        <div className='z-10 w-[400px] h-[400px] border-4 border-neutral-900 rounded-3xl bg-neutral-300 backdrop-blur-sm overflow-hidden shadow-inner' style={{ boxShadow: 'inset 0 10px 60px rgba(0, 0, 0, 1)' }}>
-          <div className={`flex flex-col justify-center items-center w-full h-full font-black font-['sixtyFourConv']`}>
-            <VelocityScroll text='In Development' className='text-3xl text-neutral-800' />
-            <VelocityScroll text='In Development' className='text-3xl text-neutral-800' />
-            <VelocityScroll text='In Development' className='text-3xl text-neutral-800' />
-            <VelocityScroll text='In Development' className='text-3xl text-neutral-800' />
-            <VelocityScroll text='In Development' className='text-3xl text-neutral-800' />
-            <VelocityScroll text='In Development' className='text-3xl text-neutral-800' />
+      <div className='relative flex items-center justify-center h-screen w-screen overflow-hidden bg-neutral-300'>
+        <div className='fixed left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 w-[calc(100vw-10px)] h-[calc(100vh-10px)] bg-black rounded-2xl shadow-md p-4 overflow-hidden' style={{ boxShadow: '0px 0px 10px 6px rgba(0,0,0, 0.3)' }}>
+          <DotPattern className={cn(
+            "opacity-50 md:[mask-image:radial-gradient(500px_circle_at_center,white,transparent)] [mask-image:radial-gradient(340px_circle_at_center,white,transparent)]",
+          )} />
+        </div>
+        <div className='w-[500px] h-[260px] rounded-3xl bg-neutral-600/10 backdrop-blur-sm z-10 text-white'>
+          <div>
+            <Particles className='opacity-20' />
           </div>
         </div>
-        <Particles className='absolute top-0 left-0 -z-10 w-screen h-screen' />
       </div>
     </>
   )
