96450eb6421686a7d0b6b4ad00b65a12d1b025fa
Abhinav Mishra <abhinavmishra3322@gmail.com>
2025-06-11 21:35:12 +0530

    Refactor Home component layout by simplifying structure and removing unused elements

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,17 +8,8 @@ import React from 'react'
 const Home = () => {
   return (
     <>
-      <div className='relative flex items-center justify-center h-screen w-screen overflow-hidden bg-neutral-300'>
-        <div className='fixed left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 w-[calc(100vw-10px)] h-[calc(100vh-10px)] bg-black rounded-2xl shadow-md p-4 overflow-hidden' style={{ boxShadow: '0px 0px 10px 6px rgba(0,0,0, 0.3)' }}>
-          <DotPattern className={cn(
-            "opacity-50 md:[mask-image:radial-gradient(500px_circle_at_center,white,transparent)] [mask-image:radial-gradient(340px_circle_at_center,white,transparent)]",
-          )} />
-        </div>
-        <div className='w-[500px] h-[260px] rounded-3xl bg-neutral-600/10 backdrop-blur-sm z-10 text-white'>
-          <div>
-            <Particles className='opacity-20' />
-          </div>
-        </div>
+      <div className='h-screen w-screen bg-black'>
+        <p className='text-white'>yooo</p>
       </div>
     </>
   )
