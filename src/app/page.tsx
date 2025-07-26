113a4f6a5654540e8842f44e1440808089bbf9a5
Abhinav Mishra <abhinavmishra3322@gmail.com>
2025-07-26 20:56:31 +0530

    new

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,11 @@
 'use client';
-import RotatingBox from "@/components/RotatingBox";
-import { Environment, OrbitControls, SpotLight, Text } from "@react-three/drei";
-import { Canvas } from "@react-three/fiber";
-import * as THREE from "three";
-
 export default function Home() {
 
 
   return (
     <>
       <div className='relative h-screen w-screen bg-black'>
-        <Canvas
-          gl={{ toneMapping: THREE.ACESFilmicToneMapping }}
-          camera={{ position: [0, 0, 5], fov: 75 }} className='h-full w-full'
-        >
-          <ambientLight intensity={0.1} />
-          <directionalLight position={[5, 5, 5]} intensity={10} />
-          <Environment preset="night" />
-            <RotatingBox />
-
-          <OrbitControls />
-
-        </Canvas>
+        <h1 className="text-white">Hello</h1>
       </div>
     </>
   );
